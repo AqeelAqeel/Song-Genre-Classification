a3609a4cec3bf9d83264a6484d312532798243cb
AqeelAqeel <aqeelali0312@gmail.com>
2021-07-22 00:27:36 -0700

    Repo clean-up, branch correction

--- src/data-acquisition-2.py
+++ src/data-acquisition-2.py
@@ -1,17 +1,18 @@
 import requests
 import os
 
+print(os.getcwd())
 
 genres = ['hip-hop', 'classical', 'country', 'electronic', 'metal']
 
-n_pages = 38
-
+n_pages = 40
+start_page = 1
 
 def write_to_file(url, genre, i):
     local_filename = url.split('/')[-1]
     # NOTE the stream=True parameter
     r = requests.get(url, stream=True)
-    with open("{}/{}/{:04d}{}".format("Data", genre, i,local_filename), 'wb') as f:
+    with open("{}/{}/{:04d}{}".format("data", genre, i,local_filename), 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024): 
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
@@ -20,8 +21,14 @@ def write_to_file(url, genre, i):
 
 # go through number of pages for each genre from parent website 
 for genre in genres:
-    counter = 1
-    for i in range(1, n_pages+1):
+    
+    counter =  1
+
+    # if genre != 'electronic':
+    #     counter = 1
+    #     start_page = 1
+    
+    for i in range(start_page, n_pages+1):
         if i == 1:
             url = f'https://elements.envato.com/audio/genre-{genre}'
         else:
@@ -34,13 +41,12 @@ for genre in genres:
         # based on string of HTML source code, split on the pattern of desired URL links per page (which come after 'src')
         response_split = response.split("src=")
 
-
         for split in response_split:
             try:
                 # look to see what index of string url contains 'mp3' to identify split location from html url link 
                 idx = split.index('mp3')
                 mp3_url = split[idx-78:idx+3]
-                print(mp3_url)
+                print(f"Currently on page {i}!")
                 #make sure the url contains the audio file
                 if mp3_url.startswith('https:'):
                     #save file to specified local drive
@@ -59,8 +65,7 @@ for each folder:
 
 while in terminal in subdirectory for each genre of musicData folder:
 
-for i in *.mp3; do ffmpeg -i $i -ss 00:00:00 -to 00:00:45 -c copy sh$i  ;done
-
+for i in *.mp3; do ffmpeg -i $i -ss 00:00:00 -to 00:00:30 -c copy shortened_$i  ;done
 
 '''
 
